08aef7f75b499bcfcf6080349f22124362ca7ce3
Stefan Quest <stefan.quest@gmail.com>
2019-05-14 19:28:43 +0200

    how to use props and pass from parent to child

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = ()=>  {
+    return (
+      <div className="ui container comments">
+       <CommentDetail  image={faker.image.avatar()} author = "Sam" timeAgo = "Today at 4.45PM" comment= {faker.lorem.sentence()}/>
+       <CommentDetail  image={faker.image.avatar()} author = "Alex" timeAgo = "Today at 2:00AM" comment= {faker.lorem.sentence()}/>
+       <CommentDetail  image={faker.image.avatar()} author = "Jane" timeAgo = "Yesterday at 5:00PM" comment= {faker.lorem.sentence()}/>
+     
+     </div>
+    );
+};
+
+ReactDOM.render(
+    <App /> , document.querySelector('#root')
+);
